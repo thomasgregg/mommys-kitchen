db066861e005d011b3cc48e04541ff7b448f1d10
Thomas Gregg <thomas@gregg.de>
2024-12-30 18:38:34 +0100

    Update send-sms.js

--- api/send-sms.js
+++ api/send-sms.js
@@ -2,10 +2,13 @@ import twilio from 'twilio';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    const { mealName, mealTime } = req.body;
+    console.log('Environment Variables:');
+    console.log('TWILIO_ACCOUNT_SID:', process.env.TWILIO_ACCOUNT_SID);
+    console.log('TWILIO_AUTH_TOKEN:', process.env.TWILIO_AUTH_TOKEN ? 'Exists' : 'Missing');
+    console.log('TWILIO_PHONE_NUMBER:', process.env.TWILIO_PHONE_NUMBER);
+    console.log('PREPARER_PHONE_NUMBER:', process.env.PREPARER_PHONE_NUMBER);
 
-    console.log('Received mealName:', mealName);
-    console.log('Received mealTime:', mealTime);
+    const { mealName, mealTime } = req.body;
 
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
