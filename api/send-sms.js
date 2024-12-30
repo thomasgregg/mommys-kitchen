a5f7c9a8463dfc3f99f596353487bc340f1285ad
Thomas Gregg <thomas@gregg.de>
2024-12-30 18:34:07 +0100

    Update send-sms.js

--- api/send-sms.js
+++ api/send-sms.js
@@ -4,6 +4,9 @@ export default async (req, res) => {
   if (req.method === 'POST') {
     const { mealName, mealTime } = req.body;
 
+    console.log('Received mealName:', mealName);
+    console.log('Received mealTime:', mealTime);
+
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
@@ -21,8 +24,8 @@ export default async (req, res) => {
       console.log(`SMS sent: ${message.sid}`);
       res.status(200).json({ message: 'SMS sent successfully!' });
     } catch (error) {
-      console.error('Error sending SMS:', error);
-      res.status(500).json({ error: 'Failed to send SMS' });
+      console.error('Error sending SMS:', error.message);
+      res.status(500).json({ error: 'Failed to send SMS', details: error.message });
     }
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
